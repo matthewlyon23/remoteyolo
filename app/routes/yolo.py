0d5c88b8db186a5d3e8275c9bc1f1938b3820e24
Matthew Lyon <75092798+matthewlyon23@users.noreply.github.com>
2025-06-25 15:37:02 +0100

    Update yolo.py

--- app/routes/yolo.py
+++ app/routes/yolo.py
@@ -21,16 +21,20 @@ class YOLOAnalysisManager:
     yolo_model_variant: YOLOModel = YOLOModel.YOLO11N
 
     def __load_yolo_variant__(yolo_model: YOLOModel, yolo_model_format: YOLOModelFormat):
-        match yolo_model_format:
-            case YOLOModelFormat.ONNX:
-                YOLOAnalysisManager.yolo_model = YOLO(f"{yolo_model}.onnx")
-            case YOLOModelFormat.NCNN:
-                YOLOAnalysisManager.yolo_model = YOLO(f"{yolo_model}_ncnn_model")
-            case _:
-                YOLOAnalysisManager.yolo_model = YOLO(yolo_model)
-        YOLOAnalysisManager.yolo_model_format = yolo_model_format
-        YOLOAnalysisManager.yolo_model_variant = yolo_model
-    
+        try:
+            match yolo_model_format:
+                case YOLOModelFormat.ONNX:
+                    YOLOAnalysisManager.yolo_model = YOLO(f"{yolo_model}.onnx")
+                case YOLOModelFormat.NCNN:
+                    YOLOAnalysisManager.yolo_model = YOLO(f"{yolo_model}_ncnn_model")
+                case _:
+                    YOLOAnalysisManager.yolo_model = YOLO(yolo_model)
+            YOLOAnalysisManager.yolo_model_format = yolo_model_format
+            YOLOAnalysisManager.yolo_model_variant = yolo_model
+        except:
+            model = YOLO(yolo_model)
+            model.export(format=yolo_model_format)
+            YOLOAnalysisManager.__load_yolo_variant__(yolo_model, yolo_model_format)
     def do_yolo_analysis(yolo_model: YOLOModel, format: YOLOModelFormat, image: Image) -> Results:
         if yolo_model!=YOLOAnalysisManager.yolo_model_variant or format!=YOLOAnalysisManager.yolo_model_format:
             YOLOAnalysisManager.__load_yolo_variant__(yolo_model, format)

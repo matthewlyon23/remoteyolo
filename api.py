0a7b5eab80c93616343cea241c5c3eefef86a83c
Matthew Lyon <lyonmatthew2003@hotmail.com>
2025-07-02 15:04:27 +0100

    Added some stuff

--- api.py
+++ api.py
@@ -5,6 +5,7 @@ import io
 from PIL.Image import Image, open
 from yolo import YOLOModelFormat, YOLOModel, YOLOAnalysisManager
 from ultralytics.engine.results import Results
+from datetime import datetime
 
 @dataclass
 class YOLOAnalysisRequest:
@@ -40,7 +41,8 @@ app = Application()
 @post("/api/analyse")
 @cache_control(no_cache=True, no_store=True)
 async def analyse_image(request: Request):
-    form: dict[str, any] = await request.form()
+    start = datetime.now()
+    form: dict = await request.form() # type: ignore
     
     format = form.get("format", YOLOModelFormat.NCNN)
     model = form.get("model", YOLOModel.YOLO11N)
@@ -48,14 +50,16 @@ async def analyse_image(request: Request):
     if "image" not in form:
         return status_code(400, {"success": False, "error": "You must provide an image"})    
     
-    request: YOLOAnalysisRequest = YOLOAnalysisRequest(model, format, form["image"])
+    analysis_request: YOLOAnalysisRequest = YOLOAnalysisRequest(model, format, form["image"])
     
-    if request.model == "undefined":
+    if analysis_request.model == "undefined":
         return status_code(400, {"success": False, "error": "The provided YOLO model is not supported. Please use one of " + ", ".join([f"'{i.lower()}'" for i in list(YOLOModel.__members__.keys())])})
         
-    if request.format == "undefined":
+    if analysis_request.format == "undefined":
         return status_code(400, {"success": False, "error": "The provided format is not supported. Please use one of " + ", ".join([f"'{i.lower()}'" for i in list(YOLOModelFormat.__members__.keys())])})
-    result: Results = YOLOAnalysisManager.do_yolo_analysis(request.model, request.format, request.image)
-    response_data = {"success": True,  "metadata": {"speed": result.speed, "names": result.names}, "result":result.summary()}
     
+    result: Results = YOLOAnalysisManager.do_yolo_analysis(analysis_request.model, analysis_request.format, analysis_request.image)
+    end = datetime.now()
+
+    response_data = {"success": True,  "metadata": {"speed": result.speed, "names": result.names, "request":{"time_ms": (end-start).microseconds/1000}}, "result":result.summary()}
     return response_data

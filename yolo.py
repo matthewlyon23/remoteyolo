0aeb2c325305f022e67049f2557c4548de65aeb1
Matthew Lyon <lyonmatthew2003@hotmail.com>
2025-07-17 14:59:24 +0100

    Custom Model Support
    - Added custom model support via /api/custom-model and /api/analyse endpoints.
    - Added ability via /api/custom-model to upload custom model
    - Added custom model type to /api/analyse endpoint
    - Added type improvements, specifically clearer AnalysisResult type to allow error handling

--- yolo.py
+++ yolo.py
@@ -2,6 +2,7 @@ from enum import StrEnum
 from ultralytics import YOLO
 from PIL.Image import Image
 from ultralytics.engine.results import Results
+import os
 
 class YOLOModelFormat(StrEnum):
     NCNN = 'ncnn'
@@ -13,6 +14,13 @@ class YOLOModel(StrEnum):
     YOLO11S = 'yolo11s'
     YOLO11M = 'yolo11m'
     YOLO11L = 'yolo11l'
+    CUSTOM  = 'custom'
+    
+class AnalysisResult:    
+    def __init__(self, success: bool, result: Results | None, reason: str | None):
+        self.success = success
+        self.result = result
+        self.reason = reason
 
 class YOLOAnalysisManager:
 
@@ -21,7 +29,7 @@ class YOLOAnalysisManager:
     yolo_model_variant: YOLOModel = YOLOModel.YOLO11N
 
     @staticmethod
-    def __load_yolo_variant__(yolo_model: YOLOModel, yolo_model_format: YOLOModelFormat):
+    def __load_yolo_variant__(yolo_model: YOLOModel, yolo_model_format: YOLOModelFormat) -> bool:
         try:
             match yolo_model_format:
                 case YOLOModelFormat.ONNX:
@@ -29,21 +37,35 @@ class YOLOAnalysisManager:
                 case YOLOModelFormat.NCNN:
                     YOLOAnalysisManager.yolo_model = YOLO(f"{yolo_model}_ncnn_model", task="detect")
                 case _:
-                    YOLOAnalysisManager.yolo_model = YOLO(yolo_model, task="detect")
+                    YOLOAnalysisManager.yolo_model = YOLO(f"{yolo_model}.pt", task="detect")
             YOLOAnalysisManager.yolo_model_format = yolo_model_format
             YOLOAnalysisManager.yolo_model_variant = yolo_model
+            return True
         except:
-            model = YOLO(yolo_model)
+            if yolo_model == YOLOModel.CUSTOM and not os.path.exists("custom.pt"):
+                return False
+            model = YOLO(f"{yolo_model}.pt")
             model.export(format=yolo_model_format)
-            YOLOAnalysisManager.__load_yolo_variant__(yolo_model, yolo_model_format)
+            return YOLOAnalysisManager.__load_yolo_variant__(yolo_model, yolo_model_format)
     
     @staticmethod
-    def analyse_image(yolo_model: YOLOModel, format: YOLOModelFormat, image: Image) -> Results:
+    def analyse_image(yolo_model: YOLOModel, format: YOLOModelFormat, image: Image) -> AnalysisResult:
         if yolo_model!=YOLOAnalysisManager.yolo_model_variant or format!=YOLOAnalysisManager.yolo_model_format:
-            YOLOAnalysisManager.__load_yolo_variant__(yolo_model, format)
-        results = YOLOAnalysisManager.yolo_model.predict(source=image, task="detect", verbose=False, save=False)
-        return results[0]
-        
+            success = YOLOAnalysisManager.__load_yolo_variant__(yolo_model, format)
+            if not success:
+                return AnalysisResult(success, None, "Could not load YOLO model")
+        try:
+            results = YOLOAnalysisManager.yolo_model.predict(source=image, task="detect", verbose=False, save=False)
+        except:
+            return AnalysisResult(False, None, "YOLO prediction failed")
+        return AnalysisResult(True, results[0], None)
+    
+def test_model(model_file: str) -> bool:
+    try:
+        YOLO(model_file)
+        return True
+    except:
+        return False
     
     
 
